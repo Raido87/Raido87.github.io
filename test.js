acaabcf10d98a1b09c193a058dbbb7a1931bbe9d
Raido87 <silovanadazda888@gmail.com>
2026-05-12 06:15:13 +0000

    Tue, May 12, 2026, 1:15 PM +07:00

--- test.js
+++ test.js
@@ -1,2 +1,4 @@
 let body = document.querySelector("body")
 document.querySelector("#red").addEventListener('click', function() {body.style.backgroundColor="red";})
+document.querySelector("#blue").addEventListener('click', function() {body.style.backgroundColor="blue";})
+document.querySelector("#green").addEventListener('click', function() {body.style.backgroundColor="green";})
